4ecfde236d8a5caada11553547a711c01fd85dc1
blafleur327 <103611758+blafleur327@users.noreply.github.com>
2024-01-29 14:57:06 -0600

    Add files via upload

---
+++ myHTMLStuff.js
@@ -0,0 +1,199 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM Loaded!');
+});
+
+/**
+ * Simple corrected algorithm for the modulo operation.
+ * @param {int} value 
+ * @param {int} modulus 
+ * @returns int
+ */
+const modulo = (value, modulus) => {
+    let temp = value%modulus;
+    if (temp >= 0) {
+        return temp;
+    }
+    else {
+        return modulus+temp;
+    }
+};
+let current = '';
+
+/**
+ * Me Being Stupid.
+ * @param {*} element 
+ */
+const fonze = (element) => {
+    let size = 17;
+    document.addEventListener('click', (event) => {
+        if (event.target === element) {
+        element.style.fontSize = size+'px';
+        size++;
+        }
+    });
+};
+
+//HERES MY NEW STUFF Having some issues with timing! Might replace list and COLLECTOR object with STORE in back.js
+/**
+ * Constructor for a list object.
+ * @param {array} elements 
+ */
+function list(elements) {
+    this.size = elements.length;
+    this.arraylist = [...elements];
+    this.append = function (...args) {
+        this.arraylist.push(...args);
+    }
+    this.prepend = function (...args) {
+        this.arraylist = [...args,...this.arraylist];
+    }
+    this.apply = function (pass,...args) {
+        this.arraylist = this.arraylist.map(x => pass(x,...args));
+    }
+}
+
+/**
+ * Constructor for a COLLECTOR object. Collects data from dropdown and createArrayInput.
+ */
+function COLLECTOR() {
+    this.items = {
+        select: null,
+        array: new list([]),
+    };
+}
+
+let BOX = new COLLECTOR;
+
+function Dropdown(label,storage,id,type,...values) {
+    let ddown = document.createElement('select');
+    let lab = document.createElement('p');
+    lab.innerHTML = `${label}`;
+    let dis = document.createElement('p');
+    //lab.setAttribute('id',`lab${id}`);    Not sure if i need this.
+    ddown.setAttribute('id',`${id}`);
+    for (let a = 0; a < values.length; a++) {
+        let opt = document.createElement('option')
+        opt.innerHTML = `${values[a]}`;
+        ddown.appendChild(opt);
+    }
+    document.body.appendChild(lab);
+    document.body.appendChild(ddown);
+    document.body.appendChild(dis);
+    dis.innerHTML = `Modulo ${storage.items.select}:`
+    document.addEventListener('click', (event) => {
+        if (type == 'number' && event.target == ddown) {
+            storage.items.select = parseInt(event.target.value);
+        }
+        else if (type == 'text' && event.target == ddown) {
+            storage.items.select = event.target.value;
+        }
+        else if (event.target !== ddown) {
+            //Don't do anything!
+        }
+        dis.innerHTML = `Modulo ${storage.items.select}:`
+    });
+}
+
+/**
+ * Constructor for an ArrayInput object. Creates an input box. Allows keypresses 'Enter' and 'Backspace'.
+ * @param {string} label
+ * @param {COLLECTOR} storage 
+ * @param {string} type 
+ * @param {string} id 
+ */
+function ArrayInput(label,storage,type,id) {
+    let inp = document.createElement('input');
+    let labl = document.createElement('p');
+    labl.innerHTML = `${label}`;
+    inp.setAttribute('id',`${id}`);
+    inp.setAttribute('placeholder',`${type.toUpperCase()}`);
+    let dis = document.createElement('div');
+    dis.setAttribute('id',`${id}dis`);
+    dis.innerHTML = `Array: []`;
+    document.body.appendChild(labl);
+    document.body.appendChild(inp);
+    document.body.appendChild(dis);
+    document.addEventListener('keydown',(event) => {
+        let possible = document.getElementById(`${id}`).value;
+        if (event.key === 'Enter') {
+            if (type == 'number' && /^[0-9]+/.test(possible) == true) {
+                storage.items.array.append(parseInt(possible));
+            }
+            else if (type == 'number' && /^[0-9]+/.test(possible) == false) {
+                alert('Invalid!');
+            }
+            else {
+                storage.items.array.append(possible);
+            }
+            document.getElementById(`${id}`).value = '';
+            let out = `Array: [${storage.items.array.arraylist}]`;
+            dis.innerHTML = out;
+        }
+        else if (event.key === 'Backspace' && document.getElementById(`${id}`).value == '') { //if empty, remove last element from storage.
+            storage.items.array.arraylist.pop();
+            let out = `Array: [${storage.items.array.arraylist}]`;
+            dis.innerHTML = out;
+        }
+        console.log(storage.items,storage.items.array.arraylist);
+    });
+}
+
+/**
+ * My current solution for creating labels. Not the best but it sorta works?
+ * @param {array} array 
+ * @param {array} labels [[column],[row]]; 
+ * @returns restructured array
+ */
+const restructure = (array,labels) => {
+    let res = [[]];
+    res[0][0] = '';
+    if (labels[0].length !== 0) {
+        res[0].push(...labels[0]);
+    }
+    else {
+        res[0].push(array.map(x => x = ''));
+    }
+    for (let a = 0; a < array.length; a++) {
+        res.push([labels[1][a],...array[a]]);
+    }
+    return res;
+}
+
+/**
+ * Creates a table based on an input 2d array and optional labels. 
+ * @param {array} array 
+ * @param {array} labels [[column labels],[row labels]];
+ */
+function ConstructTable(array,labels = null) {
+    let modified = undefined;
+    if (labels == null) {
+        modified = array;
+    }
+    else {
+        modified = restructure(array,labels);
+    }
+    let table = document.createElement('table');
+    for (let a = 0; a < modified.length; a++) {
+        let row = document.createElement('tr');
+        for (let b = 0; b < modified[a].length; b++) {
+            let data = document.createElement('td');
+            data.innerHTML = `${modified[a][b]}`;
+            row.appendChild(data);
+        }
+        table.appendChild(row);
+    }
+    document.body.appendChild(table);
+};
+
+let drop = new Dropdown('Select Modular Universe:',BOX,'ba','number',2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24);
+let arr = new ArrayInput('Input Array Elements:',BOX,'number','blah');
+
+//import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js';
+
+let cont = document.createElement('div');
+cont.setAttribute('id','box');
+let shape = SVG.addTo(cont).size(100,100);
+
+document.body.appendChild(cont);
+
+
