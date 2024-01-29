848a9352f452d9020e6c884c303ee92c59425543
blafleur327 <103611758+blafleur327@users.noreply.github.com>
2024-01-29 15:52:40 -0600

    Add files via upload

--- myHTMLStuff.js
+++ myHTMLStuff.js
@@ -17,59 +17,18 @@ const modulo = (value, modulus) => {
         return modulus+temp;
     }
 };
-let current = '';
-
-/**
- * Me Being Stupid.
- * @param {*} element 
- */
-const fonze = (element) => {
-    let size = 17;
-    document.addEventListener('click', (event) => {
-        if (event.target === element) {
-        element.style.fontSize = size+'px';
-        size++;
-        }
-    });
-};
+//let current = '';
 
 //HERES MY NEW STUFF Having some issues with timing! Might replace list and COLLECTOR object with STORE in back.js
-/**
- * Constructor for a list object.
- * @param {array} elements 
- */
-function list(elements) {
-    this.size = elements.length;
-    this.arraylist = [...elements];
-    this.append = function (...args) {
-        this.arraylist.push(...args);
-    }
-    this.prepend = function (...args) {
-        this.arraylist = [...args,...this.arraylist];
-    }
-    this.apply = function (pass,...args) {
-        this.arraylist = this.arraylist.map(x => pass(x,...args));
-    }
-}
 
-/**
- * Constructor for a COLLECTOR object. Collects data from dropdown and createArrayInput.
- */
-function COLLECTOR() {
-    this.items = {
-        select: null,
-        array: new list([]),
-    };
-}
-
-let BOX = new COLLECTOR;
+let collect = [];   //Variable for array creation;
+let modus = null;     //Variable for dropdown;
 
 function Dropdown(label,storage,id,type,...values) {
     let ddown = document.createElement('select');
     let lab = document.createElement('p');
     lab.innerHTML = `${label}`;
     let dis = document.createElement('p');
-    //lab.setAttribute('id',`lab${id}`);    Not sure if i need this.
     ddown.setAttribute('id',`${id}`);
     for (let a = 0; a < values.length; a++) {
         let opt = document.createElement('option')
@@ -79,26 +38,26 @@ function Dropdown(label,storage,id,type,...values) {
     document.body.appendChild(lab);
     document.body.appendChild(ddown);
     document.body.appendChild(dis);
-    dis.innerHTML = `Modulo ${storage.items.select}:`
+    dis.innerHTML = `Modulo ${storage}:`
     document.addEventListener('click', (event) => {
         if (type == 'number' && event.target == ddown) {
-            storage.items.select = parseInt(event.target.value);
+            storage = parseInt(event.target.value);
         }
         else if (type == 'text' && event.target == ddown) {
-            storage.items.select = event.target.value;
+            storage = event.target.value;
         }
         else if (event.target !== ddown) {
             //Don't do anything!
         }
-        dis.innerHTML = `Modulo ${storage.items.select}:`
+        dis.innerHTML = `Modulo ${storage}:`
     });
 }
 
 /**
  * Constructor for an ArrayInput object. Creates an input box. Allows keypresses 'Enter' and 'Backspace'.
  * @param {string} label
- * @param {COLLECTOR} storage 
- * @param {string} type 
+ * @param {array} storage = empyt array [];
+ * @param {string} type = "number"/"text";
  * @param {string} id 
  */
 function ArrayInput(label,storage,type,id) {
@@ -107,34 +66,35 @@ function ArrayInput(label,storage,type,id) {
     labl.innerHTML = `${label}`;
     inp.setAttribute('id',`${id}`);
     inp.setAttribute('placeholder',`${type.toUpperCase()}`);
-    let dis = document.createElement('div');
-    dis.setAttribute('id',`${id}dis`);
-    dis.innerHTML = `Array: []`;
+    let display = document.createElement('div');
+    display.setAttribute('id',`${id}dis`);
+    display.innerHTML = `Array: []`;
     document.body.appendChild(labl);
     document.body.appendChild(inp);
-    document.body.appendChild(dis);
+    document.body.appendChild(display);
     document.addEventListener('keydown',(event) => {
         let possible = document.getElementById(`${id}`).value;
         if (event.key === 'Enter') {
             if (type == 'number' && /^[0-9]+/.test(possible) == true) {
-                storage.items.array.append(parseInt(possible));
+                storage.push(modulo(parseInt(possible),modus));//storage.items.array.append(parseInt(possible));
+                console.log(parseInt(possible));
             }
             else if (type == 'number' && /^[0-9]+/.test(possible) == false) {
                 alert('Invalid!');
             }
             else {
-                storage.items.array.append(possible);
+                storage.push(possible);//storage.items.array.append(possible);
             }
             document.getElementById(`${id}`).value = '';
-            let out = `Array: [${storage.items.array.arraylist}]`;
-            dis.innerHTML = out;
+            let out = `Array: [${storage}]`//storage.items.array.arraylist}]`;
+            display.innerHTML = out;
         }
         else if (event.key === 'Backspace' && document.getElementById(`${id}`).value == '') { //if empty, remove last element from storage.
-            storage.items.array.arraylist.pop();
-            let out = `Array: [${storage.items.array.arraylist}]`;
-            dis.innerHTML = out;
+            storage.pop();//storage.items.array.arraylist.pop();
+            let out = `Array: [${storage}]`;
+            display.innerHTML = out;
         }
-        console.log(storage.items,storage.items.array.arraylist);
+        console.log(storage);
     });
 }
 
@@ -185,15 +145,9 @@ function ConstructTable(array,labels = null) {
     document.body.appendChild(table);
 };
 
-let drop = new Dropdown('Select Modular Universe:',BOX,'ba','number',2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24);
-let arr = new ArrayInput('Input Array Elements:',BOX,'number','blah');
+let drop = new Dropdown('Select Modular Universe:',modus,'ba','number',2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24);
+let arr = new ArrayInput('Input Array Elements:',collect,'number','blah');
 
-// import { SVG, extend as SVGextend, Element as SVGElement } from 'svg.js';
 
-let cont = document.createElement('div');
-cont.setAttribute('id','box');
-let shape = SVG().addTo(cont).size(100,100);
-
-document.body.appendChild(cont);
 
 
